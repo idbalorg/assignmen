c534d692f34b98dd0bb194517d53ba07302e93f6
idbalorg <108686204+idbalorg@users.noreply.github.com>
2023-02-12 00:24:06 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function multiply (num1, num2) {
+    return (num1 * num2);
+}
+
+console.log(multiply(5,6));
+
+let firstName = "Idris";
+let lastName = "Balogun";
+let stuternTrack = "Front End web development";
+let skills = "HTML and CSS";
+let favoriteColor = "i don't have";
+let countryIndependenceYear = 1960;
+
+console.log("My name is " + firstName + " " + lastName + ", my stutern track is " + stuternTrack + ", before now, i have been able to get some level of expertise in " + skills + " unfortunately " + favoriteColor + " a favourite color. I am from Nigeria and my country got independence in " + countryIndependenceYear +".");
